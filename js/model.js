ec66553145cef3fcaba625a676034aaa43c7f535
yji35j42012 <35093568+yji35j42012@users.noreply.github.com>
2026-05-18 13:48:25 +0800

    rwd

--- js/model.js
+++ js/model.js
@@ -11,11 +11,21 @@ menuBtn.addEventListener("click", function () {
 	header.classList.toggle("showMenu");
 })
 
+const back_home = document.querySelector("#back_home");
+const back_prev = document.querySelector("#back_prev");
 const lv2 = document.querySelector("#lv2");
+const lv2_list = document.querySelectorAll("#lv2 .lv2_list");
+var lv2_count = 0;
+
+function close_lv2_list(params) {
+	for (let i = 0; i < lv2_list.length; i++) {
+		lv2_list[i].classList.remove("on");
+	}
+}
+
 if (lv2) {
-	const lv2_list = document.querySelectorAll("#lv2 .lv2_list");
 	const lv2_box_listOutside = document.querySelector("#lv2_box_listOutside");
-	var lv2_count = 0;
+
 	for (let i = 0; i < lv2_list.length; i++) {
 		const element = lv2_list[i];
 		element.onclick = function () {
@@ -24,6 +34,36 @@ if (lv2) {
 			lv2_count = i;
 			var move = -100 * i + "%"
 			lv2_box_listOutside.style = `transform: translateY( ${move} );`;
+			if (dev == "ph") {
+				back_home.style.display = "none";
+				back_prev.style.display = "block";
+			}
 		}
 	}
 }
+if (back_home) {
+	back_prev.onclick = function () {
+		back_home.style.display = "block";
+		back_prev.style.display = "none";
+		close_lv2_list();
+	}
+}
+
+
+var dev = window.innerWidth >= 995 ? "pc" : "ph";
+if (dev == "ph") {
+	close_lv2_list();
+}
+window.addEventListener('resize', function () {
+	console.log('dev', dev);
+	// console.log(lv2_list);
+	if (window.innerWidth <= 995 && dev == "pc" && lv2_list) {
+		close_lv2_list();
+	}
+	if (window.innerWidth > 995 && dev == "pc" && lv2_list) {
+		lv2_list[lv2_count].classList.add("on");
+	}
+	window.innerWidth >= 995 ? dev = "pc" : dev = "ph"
+	// console.log('目前視窗寬度：', window.innerWidth);
+});
+
